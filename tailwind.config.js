29809c1ba5cef92637a03353e9df5593aed935c1
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-03-15 19:18:47 +0530

    chore: Add colourscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				red: '#EB4747',
+				green: '#70E9A1',
+				accent: '#71C9DA',
+				background: '#D9D9D9',
+				foreground: '#1A1A1A'
+			}
+		}
 	},
-	plugins: [],
-}
-
+	plugins: []
+};
