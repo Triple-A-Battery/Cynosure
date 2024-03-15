3eba344ecadca8995687e26342278c8500efaf0a
Aditya Jyoti <aj.adityajyoti@gmail.com>
2024-03-15 20:36:26 +0530

    Feat: Make navbar and footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<footer
+	class="bg-background text-foreground w-full flex flex-col items-center justify-center p-4 gap-2"
+>
+	<div class="border-t-2 border-foreground border-solid w-full mb-2"></div>
+	<div class="text-2xl font-bold">Cynosure</div>
+	<div class="text-lg flex gap-4">
+		<div>Aditya Jyoti</div>
+		<div>Anumeya Sehgal</div>
+		<div>Aadivishnu Gajendra</div>
+		<div>Aditya Dave</div>
+	</div>
+	<div class="text-sm font-bold">&copy; Triple A Battery, 2024-25</div>
+</footer>
