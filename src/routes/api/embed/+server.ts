feda5dff29ab3007b6d57f767e1ee99554b7d313
mradigen <55953083+mradigen@users.noreply.github.com>
2024-03-15 17:41:02 +0530

    add: /api/embed

--- src/routes/api/embed/+server.ts
+++ src/routes/api/embed/+server.ts
@@ -1,7 +1,22 @@
+import { EmbeddingModel, FlagEmbedding } from "fastembed";
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
 	const { tasks } = await request.json();
 
-	return json(tasks);
+	if (!tasks) return new Response();
+
+	const embeddingModel = await FlagEmbedding.init({
+		model: EmbeddingModel.BGESmallENV15
+	});
+
+	const embeddings = embeddingModel.embed(tasks);
+	let res = [];
+
+	for await (const batch of embeddings) {
+		for (const embed of batch) {
+			res.push(Object.values(embed));
+		}
+	}
+	return json(res);
 }
