638a7db211a9da70d0819b6693157d83860a7ecd
mradigen <55953083+mradigen@users.noreply.github.com>
2024-03-15 18:26:54 +0530

    fix: Allow unsafe storage access

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { set, get } from '$lib/storage';
+	import { getEmbedding } from '$lib/embedding';
+
+	let task: string;
+
+	async function save() {
+		let embedding = (await getEmbedding([task]))[0];
+		set('task', task);
+		set('taskEmbedding', embedding);
+	}
+
+	onMount(async () => {
+		chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
+		task = await get('task');
+	});
+</script>
+
+<p>I am going to focus on:</p>
+<input placeholder="Programming" bind:value={task} />
+<button on:click={save}>now</button>
